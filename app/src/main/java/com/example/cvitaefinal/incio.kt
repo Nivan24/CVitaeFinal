69b54acfe0421c3838c9320b235a8945a26fa43f
Santiago Palomar <124805965+SantiagoPalomar@users.noreply.github.com>
2024-07-09 10:15:06 -0500

    Se acomodaron los botones y los inputs

---
+++ app/src/main/java/com/example/cvitaefinal/incio.kt
@@ -0,0 +1,17 @@
+package com.example.cvitaefinal
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class incio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_incio)
+
+
+    }
+}
