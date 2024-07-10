79edadf7983c863dd3f9133740514100080baf12
Nicolas <175035962+Nivan24@users.noreply.github.com>
2024-07-09 20:57:34 -0500

    Se agrega eventos

--- app/src/main/java/com/example/cvitaefinal/registro.kt
+++ app/src/main/java/com/example/cvitaefinal/registro.kt
@@ -7,12 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_registro)
+
+        //Analytic Event
+        val analytics:FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integracion de Firebase completa")
+        analytics.logEvent("InitScreen", bundle)
 
 
 
